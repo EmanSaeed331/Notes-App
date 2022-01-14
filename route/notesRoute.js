722a0e80bfeb8dc53cd72870e3e5fcb2a6fa4ebc
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-14 05:41:41 +0200

    add:routes notes

---
+++ route/notesRoute.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const router = express.Router()
+
+router.get("/notes",function(req,res){
+    res.send('Get All Notes');
+})
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const noteRouter = require('./route/notesRoute')
 const app = express();
 
-const port = 8880;
+const port = 8008;
 
 
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.use('/api/v1',noteRouter)
 app.get("/",function(req,res){
     res.send('Server starting.........');
 });
