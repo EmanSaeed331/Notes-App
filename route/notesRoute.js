e4f029553d8599e348a63a3bd51950c2a545c2f7
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-14 06:11:37 +0200

    add:routes

--- route/notesRoute.js
+++ route/notesRoute.js
@@ -3,6 +3,10 @@ const router = express.Router()
 var noteController = require('../controller/notesController')
 
 router.get("/notes",noteController.getAllNotes)
+router.post("/notes/save",noteController.saveNotes)
+router.put("/notes/update",noteController.updateNotes)
+router.delete("/notes/delete",noteController.deleteNote)
+
 
 
 module.exports = router
