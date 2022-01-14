5b4fa04a7e71b0cae9a92ed713a253b7a9f464e5
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-14 06:09:03 +0200

    add:controller methods

--- controller/notesController.js
+++ controller/notesController.js
@@ -1,3 +1,12 @@
 exports.getAllNotes = function(req,res){
     res.send("All Notes send .........");
 }
+exports.saveNotes = function(req,res){
+    res.send("Save note  .........");
+}
+exports.updateNotes = function(req,res){
+    res.send("update note .........");
+}
+exports.deleteNote = function(req,res){
+    res.send("delete note .........");
+}
