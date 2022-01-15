4a3faf2302e0a98cd8f9f0d92073dfb597de3143
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-15 07:01:02 +0200

    add:model to controller

--- controller/notesController.js
+++ controller/notesController.js
@@ -1,12 +1,21 @@
 var generator = require('../util/generator');
 var memStorage = require('../util/memoryStorage');
+var model = require('../model/note.model');
 exports.getAllNotes = function(req,res){
     var noteId = generator.generate();
     memStorage.store.setItem(noteId,"1st note id");
     var keys =memStorage.getKeys(memStorage.store);
     var values =memStorage.getValues(memStorage.store)
+    var Note = model.Note;
+    var noteObj = new Note(
+        noteId,
+        "Note 1",
+        "Content1",
+        "CreatedBy",
+        "CreatedOn",
 
-    res.send("All Notes send ........."+JSON.stringify(keys));
+    );
+    res.send("All Notes send ........."+JSON.stringify(noteObj));
 }
 exports.saveNotes = function(req,res){
     res.send("Save note  .........");
