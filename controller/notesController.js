31b3eb0827623f32e9a8a9aee0c39c9c393c41f5
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-01-15 08:44:42 +0200

    add:complete getAllNote

--- controller/notesController.js
+++ controller/notesController.js
@@ -2,20 +2,21 @@ var generator = require('../util/generator');
 var memStorage = require('../util/memoryStorage');
 var model = require('../model/note.model');
 exports.getAllNotes = function(req,res){
-    var noteId = generator.generate();
-    memStorage.store.setItem(noteId,"1st note id");
-    var keys =memStorage.getKeys(memStorage.store);
-    var values =memStorage.getValues(memStorage.store)
-    var Note = model.Note;
-    var noteObj = new Note(
-        noteId,
-        "Note 1",
-        "Content1",
-        "CreatedBy",
-        "CreatedOn",
+    // var noteId = generator.generate();
+    // memStorage.store.setItem(noteId,"1st note id");
+    // var keys =memStorage.getKeys(memStorage.store);
+     var values =memStorage.getValues(memStorage.store);
+     res.status(200).send(JSON.stringify(values));
+    // var Note = model.Note;
+    // var noteObj = new Note(
+       //  noteId,
+       //  "Note 1",
+         // "Content1",
+        // "CreatedBy",
+         // "CreatedOn",
 
-    );
-    res.send("All Notes send ........."+JSON.stringify(noteObj));
+   // );
+
 }
 exports.saveNotes = function(req,res){
     var seqId = generator.generate();
